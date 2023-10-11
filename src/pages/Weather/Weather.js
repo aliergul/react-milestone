c70e6ef5ec5d6b011a4883b295da99ffa2e64f88
ali <ali.ergul@pusula.net.tr>
2023-10-11 15:29:22 +0300

    Adding weather search component

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,7 +1,16 @@
 import React from "react";
+import WeatherSearch from "./WeatherSearch";
 
 function Weather() {
-  return <div>Weather</div>;
+  const handleSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
+  return (
+    <div className="max-w-3xl m-5 mx-auto">
+      <WeatherSearch onSearchChange={handleSearchChange} />
+    </div>
+  );
 }
 
 export default Weather;
---
+++ src/pages/Weather/WeatherApi.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4fe8e18a13msh0ace5ead775d0d1p1c6d36jsnc35ca5a5fc95",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
