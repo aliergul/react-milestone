8d658cd1818508ecc748fdf33954de92e6094b0d
ali <ali.ergul@pusula.net.tr>
2023-10-13 11:26:09 +0300

    Add To Do component

---
+++ src/pages/Todo/AddTodo.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToDo } from "../../store/todoService/todoSlice";
+
+const AddTodo = () => {
+  const dispatch = useDispatch();
+  const [state, setState] = useState({
+    content: "",
+    contentError: null,
+  });
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+      [`${e.target.name}Error`]: null,
+    });
+  };
+  const add = () => {
+    if (content === "") {
+      setState({ ...state, contentError: "Content must not be empty!" });
+      return;
+    }
+    dispatch(addToDo({ newContent: content }));
+    setState({ ...state, content: "" });
+  };
+  const { content, contentError } = state;
+  return (
+    <div className="">
+      <h2>Add Todo</h2>
+      <input
+        type="text"
+        value={content}
+        name="content"
+        onChange={handleChange}
+        className="text-black"
+      />
+      <button type="button" className="" onClick={add}>
+        Add
+      </button>
+      {contentError ? console.log({ contentError }) : console.log("All Good")}
+    </div>
+  );
+};
+
+export default AddTodo;
--- src/pages/Todo/Todo.js
+++ src/pages/Todo/Todo.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AddTodo from "./AddTodo";
 
 function Todo() {
-  return <div>Todo Page</div>;
+  return (
+    <div>
+      <AddTodo />
+    </div>
+  );
 }
 
 export default Todo;
