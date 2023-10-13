da817af7b7a07d7e33cd2a52e06872fa1d2cf4cb
ali <ali.ergul@pusula.net.tr>
2023-10-13 17:10:19 +0300

    Styling modals

--- src/pages/Todo/TodoModals/DeleteModal.js
+++ src/pages/Todo/TodoModals/DeleteModal.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { deleteToDo } from "../../../store/todoService/todoSlice";
-import { Dialog } from "@headlessui/react";
-import { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 
 function DeleteModal({ isOpen, setIsOpen, selectedTodo }) {
   const dispatch = useDispatch();
@@ -17,20 +17,63 @@ function DeleteModal({ isOpen, setIsOpen, selectedTodo }) {
     }
   };
   return (
-    <Dialog
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50"
-    >
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4 text-black">
-        <Dialog.Panel className="w-full max-w-sm rounded bg-white">
-          <Dialog.Title>Delete Task</Dialog.Title>
-          <Dialog.Description>Are you sure?</Dialog.Description>
-          <button onClick={handleDelete}>Yes</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        onClose={() => setIsOpen(false)}
+        as="div"
+        className="relative z-10"
+      >
+        <Transition.Child
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+          as={Fragment}
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+              as={Fragment}
+            >
+              <Dialog.Panel className="grid gap-y-4 w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900 flex justify-center"
+                >
+                  Are you sure to delete task?
+                </Dialog.Title>
+                <div className="grid gap-y-3">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                    onClick={handleDelete}
+                  >
+                    Yes
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
 export default DeleteModal;
--- src/pages/Todo/TodoModals/EditModal.js
+++ src/pages/Todo/TodoModals/EditModal.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import React from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
 import { editToDo } from "../../../store/todoService/todoSlice";
 
@@ -26,29 +27,69 @@ function EditModal({ isOpen, setIsOpen, selectedTodo }) {
   };
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50"
-    >
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4 text-black">
-        <Dialog.Panel className="w-full max-w-sm rounded bg-white">
-          <Dialog.Title>Edit Task</Dialog.Title>
-          <Dialog.Description>
-            You cand edit the task you choose.
-          </Dialog.Description>
-          <input
-            name="content"
-            type="text"
-            className="text-black"
-            value={content}
-            onChange={handleChange}
-          />
-          <button onClick={() => handleEdit()}>Edit</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        as="div"
+        className="relative z-10"
+      >
+        <Transition.Child
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+          as={Fragment}
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+              as={Fragment}
+            >
+              <Dialog.Panel className="grid gap-y-4 w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900 flex justify-center"
+                >
+                  Edit Task
+                </Dialog.Title>
+                <input
+                  name="content"
+                  type="text"
+                  className="text-black"
+                  value={content}
+                  onChange={handleChange}
+                />
+                <div className="grid gap-y-3">
+                  <button
+                    onClick={() => handleEdit()}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => setIsOpen(false)}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
 export default EditModal;
