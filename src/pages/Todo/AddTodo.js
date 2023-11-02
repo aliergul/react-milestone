7b30524b0ef2f2d5dc23a677c1490d4bed9d36b8
ali <ali.ergul@pusula.net.tr>
2023-11-02 11:16:51 +0300

    Added snackbars to todo page

--- src/pages/Todo/AddTodo.js
+++ src/pages/Todo/AddTodo.js
@@ -7,6 +7,8 @@ import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
 import i18n from "../../i18n/i18n";
 import { Tooltip } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
 import { db } from "../../firebase";
 import { set, ref } from "firebase/database";
@@ -16,6 +18,8 @@ const AddTodo = () => {
 
   const [state, setState] = useState({
     content: "",
+    snackbarSuccessOpen: false,
+    snackbarFailOpen: false,
   });
   const handleChange = (e) => {
     setState({
@@ -23,6 +27,10 @@ const AddTodo = () => {
       [e.target.name]: e.target.value,
     });
   };
+  const handleSnackbarClose = () => {
+    setState({ ...state, snackbarSuccessOpen: false, snackbarFailOpen: false });
+  };
+
   const add = () => {
     if (content === "") {
       setState({ ...state });
@@ -40,15 +48,16 @@ const AddTodo = () => {
     set(newTodoRef, newTodo)
       .then(() => {
         dispatch(addToDo({ newContent: content }));
-        setState({ ...state, content: "" });
+        setState({ ...state, content: "", snackbarSuccessOpen: true });
       })
       .catch((error) => {
+        setState({ ...state, snackbarFailOpen: true });
         console.error("Veri eklenirken hata oluştu:", error);
       });
 
     setState({ ...state, content: "" });
   };
-  const { content } = state;
+  const { content, snackbarSuccessOpen, snackbarFailOpen } = state;
 
   return (
     <Box
@@ -78,6 +87,32 @@ const AddTodo = () => {
           </IconButton>
         </Tooltip>
       </div>
+      <Snackbar
+        open={snackbarSuccessOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success">
+          {i18n.t("snackbars:todo_add")}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={snackbarFailOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="error">
+          {i18n.t("snackbars:todo_error")}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
